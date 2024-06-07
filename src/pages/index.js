7d700cac0b4a31ca439354ff155be7ce3c9aea6f
Parth Jain <66227450+parthjain1402@users.noreply.github.com>
2024-06-07 20:13:28 +0530

    Add files via upload

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
 import Seo from '../components/seo'
-import { ExperienceEntry, EducationEntry, PublicationEntry } from '../components/curriculumEntry';
+import { ProjectsEntry, EducationEntry, PublicationEntry, ConferenceEntry } from '../components/curriculumEntry';
 import { Link } from 'gatsby';
 import * as icons from 'react-icons/ri';
 import scrollTo from 'gatsby-plugin-smoothscroll';
@@ -29,9 +29,6 @@ const IndexPage = () => {
             <Link href="https://www.twitter.com/" className='hover:scale-125 cursor-pointer transition ease-in-out duration-300' aria-label="Twitter">
               <icons.RiTwitterFill size={32}></icons.RiTwitterFill>
             </Link>
-            <Link href="https://scholar.google.com/" target='_blank' className='hover:scale-125 cursor-pointer transition ease-in-out duration-300' aria-label='Google Scholar'>
-              <i className="ai ai-google-scholar ai-size-32"></i>
-            </Link>
             <Link href="https://drive.google.com/" target='_blank' className='hover:scale-125 cursor-pointer transition ease-in-out duration-300' aria-label='CV'>
               <i className="ai ai-cv ai-size-32"></i>
             </Link>
@@ -41,15 +38,19 @@ const IndexPage = () => {
           <div className='hidden lg:flex flex-col w-1/3 items-start'>
             <div className='flex flex-row place-items-center group'>
               <hr className='w-6 group-hover:w-8 mr-2 border-1 border-slate-800 dark:border-white group-transition ease-in-out duration-300'></hr>
-              <button onClick={() => scrollTo("#experience")} className='peer group-hover:underline'>Experience</button>
+              <button onClick={() => scrollTo("#education")} className='peer group-hover:underline'>Education</button>
             </div>
             <div className='flex flex-row place-items-center group'>
               <hr className='w-6 group-hover:w-8 mr-2 border-1 border-slate-800 dark:border-white group-transition ease-in-out duration-300'></hr>
-              <button onClick={() => scrollTo("#education")} className='peer group-hover:underline'>Education</button>
+              <button onClick={() => scrollTo("#projects")} className='peer group-hover:underline'>Projects</button>
             </div>
             <div className='flex flex-row items-center group'>
                 <hr className='w-6 group-hover:w-8 mr-2 border-1 border-slate-800 dark:border-white group-transition ease-in-out duration-300'></hr>
                 <button onClick={() => scrollTo("#publications")} className='peer hover:underline'>Publications</button>
+            </div>
+            <div className='flex flex-row items-center group'>
+              <hr className='w-6 group-hover:w-8 mr-2 border-1 border-slate-800 dark:border-white group-transition ease-in-out duration-300'></hr>
+              <button onClick={() => scrollTo("#conference")} className='peer hover:underline'>Conferences</button>
             </div>
             <div className='flex flex-row items-center group'>
               <hr className='w-6 group-hover:w-8 mr-2 border-1 border-slate-800 dark:border-white group-transition ease-in-out duration-300'></hr>
@@ -60,89 +61,109 @@ const IndexPage = () => {
 
         {/* Text and content */}
         <div className="container flex flex-col">
-          <h1 className="text-3xl font-bold mb-4">Hi, I'm Author!</h1>
+          <h1  className="text-3xl font-bold mb-4">Hi, I'm Purtee Kohli!</h1>
 
           <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-            fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
-            mollit anim id est laborum.
+          Moving form Polaris software labs ,Noida after 4 yrs from development to teaching.
+          It was a a great shift with a very enriching experience , Try to teach, mentor and moulds the young minds.
+           
           </p>
           <p className="mb-4">
-            Aliquam ac erat suscipit ipsum feugiat sollicitudin quis ut risus. Suspendisse id ipsum at justo 
-            facilisis luctus. Suspendisse eleifend ullamcorper massa, nec malesuada tortor ornare eget. 
-            Curabitur tristique tincidunt odio, id tincidunt mauris semper in. 
+          She received her M.Phil degree in Computer Science and is planning to pursue a doctorate in the near future. 
+          Her research interests are Software Engineering,Digital Image Processing and Multimedia.
+          She also holds a Masters degree in Computer Application from Maharishi Dayanand University, Rothak in addition to 
+          abachelor’s degree in Physics from Delhi University. Purtee originally started her career in the field of corporate IT 
+          working with Polaris Software labs,Noida forclients like ICICI India, Bank Artha Graya , Indonesia.  
           </p>
+          
           <p className="mb-4">
-            Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In hac 
-            habitasse platea dictumst. Donec eu turpis sapien. Suspendisse imperdiet urna a vehicula aliquet. 
-            Vestibulum vel elementum enim, in ultricies arcu. Interdum et malesuada fames ac ante ipsum primis in 
-            faucibus. Mauris ac cursus eros, varius fermentum massa.
+          After proving herself as an accomplished software engineer for 4 years Purtee decided to pursue her career in teaching due 
+          to her deep passion for knowledge. Purtee teaches a variety of courses, including Data Structures, UNIX,ICP,OOSP, 
+          Software Engineering, Multimedia and is committed to establish herself as a reputed professional in the field of education. 
           </p>
 
           {/* Timeline */}
-          <section>
-            <div id="experience" className='-translate-y-16'/>
-            <h1 className='text-2xl py-4 font-medium'>Experience</h1>
-            <ExperienceEntry
-              date={'Jan\u00a02022 - Dec\u00a02022'}
-              title={'Job 3'}
-              company={'Company 3'}
-              description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-              tags={['Skill 1', 'Skill 2', 'Skill 3']}
-            />
-            <ExperienceEntry
-              date={'Jan\u00a02021 - Dec\u00a02021'}
-              title={'Job 2'}
-              company={'Company 2'}
-              description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-              tags={['Skill 1', 'Skill 2', 'Skill 3']}
-            />
-            <ExperienceEntry
-              date={'Jan\u00a02020 - Dec\u00a02020'}
-              title={'Job 1'}
-              company={'Company 1'}
-              description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-              tags={['Skill 1', 'Skill 2', 'Skill 3']}
-            />
-          </section>
-
+          
           <section>
             <div id="education" className='-translate-y-16'/>
             <h1 className='text-2xl py-4 font-medium'>Education</h1>
             <EducationEntry
-              date={'2021 - 2022'}
-              university={'University 2'}
-              degree={'Degree 2'}
-              description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-              tags={['Subject 1', 'Subject 2', 'Subject 3']}
+              date={'2013'}
+              university={'IIT Dehli'}
+              degree={'Project Management Course'}
             />
             <EducationEntry
-              date={'2020 - 2021'}
-              university={'University 1'}
-              degree={'Degree 1'}
-              description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
-              tags={['Subject 1', 'Subject 2', 'Subject 3']}
+              date={'2008'}
+              university={'Alappya University'}
+              degree={'Master of Philosophy (Computer Science)'}
             />
+            <EducationEntry
+              date={'2001'}
+              university={'Rothak University'}
+              degree={'Master of Computer Applications'}
+            />
+            <EducationEntry
+              date={''}
+              university={'Delhi University'}
+              degree={'Bachelor of Science Honours (Physics)'}
+            />
+          </section>
+          
+          <section>
+            <div id="projects" className='-translate-y-16'/>
+            <h1 className='text-2xl py-4 font-medium'>Projects</h1>
+            <ProjectsEntry
+              name={'Collect Now'}
+              description={'This is software to be implemented by the ICICI bank for its collection purpose. Once the loan account becomes delinquent, the amount retrieval becomes the task of the collectors they can be either field or telly collector. This software aids in automating the task allocation and status updation of the accounts.'}
+            />
+            <ProjectsEntry
+              name={'Alert Now'}
+              description={'A system developed for banks to send Alerts through e-mail /SMS to the account holders about their bill payments , credit , debit transaction in their A/C'}            />
           </section>
 
           <section>
             <div id="publications" className='-translate-y-16'/>
             <h1 className='text-2xl py-4 font-medium'>Publications</h1>
             <PublicationEntry
-              date={'2023'}
-              title={'Publication 2'}
+              name={''}
+              title={'IOSR Journal of Computer Engineering'}
               authors={'Author 1, Author 2'}
               journal={'Journal 2'}
-              tags={['Keyword 1', 'Keyword 2', 'Keyword 3']}
             />
             <PublicationEntry
               date={'2022'}
               title={'Publication 1'}
               authors={'Author 1, Author 2, Author 3'}
               journal={'Journal 1'}
-              tags={['Keyword 1', 'Keyword 2', 'Keyword 3']}
+            />
+          </section>
+
+          <section>
+            <div id="conference" className='-translate-y-16'/>
+            <h1 className='text-2xl py-4 font-medium'>Conferences Attended</h1>
+            <ConferenceEntry
+              date={'2017'}
+              title={'Cost Estimation Model'}
+              authors={'Purtee Kohli, D Soni'}
+              journal={'Cost Estimation Model for Web Applications using Agile Software Development Methodology, Pertanika Journal of Science & Technology'}
+            />
+            <ConferenceEntry
+              date={'May 2016'}
+              title={'TEAM MEMBER SELECTION IN AGILE'}
+              authors={'Purtee Kohli, Dr. Devpriya Soni, Avnish Singh Jat'}
+              journal={'IJSTM, international Journal of Science Technology and Management, Vol 5, ISSN234-1537'}
+            />
+            <ConferenceEntry
+              date={'September 2010'}
+              title={'A Risk Assessment Tool'}
+              authors={'Purtee Kohli, Chechani Niranjan'}
+              journal={ 'Fifth National IT Conference – IT Initiatives for Building Creative Organizations Indore'}
+            />
+            <ConferenceEntry
+              date={'Feburary 2008'}
+              title={'Road Map on Requirement volatility'}
+              authors={'Purtee Kohli, Mahnohar Lal'}
+              journal={'National Conference Advances in Communication, Computers,Control & Knowledge Management  ACCC-KM-08 , Bhadurgarh'}
             />
           </section>
 
@@ -153,7 +174,7 @@ const IndexPage = () => {
               If you are interested in discussing, please feel free to reach out ! You can contact me at the following email adress: 
             </p>    
             <p>
-              contact [dot] me @ email [dot] com
+              purtijk@gmail.com
             </p>
           </section>
         </div>
